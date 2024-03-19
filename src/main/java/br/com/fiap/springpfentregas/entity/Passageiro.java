900cef7c272ce3297fe5c37bfc833e758372d14d
Murilo <rm550858@fiap.com.br>
2024-03-19 09:20:05 -0300

    create class Passageiro

---
+++ src/main/java/br/com/fiap/springpfentregas/entity/Passageiro.java
@@ -0,0 +1,36 @@
+package br.com.fiap.springpfentregas.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TB_CP1_PASSAGEIRO")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Passageiro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_PASSAGEIRO")
+    @SequenceGenerator(
+            name = "SQ_PASSAGEIRO",
+            sequenceName = "SQ_PASSAGEIRO",
+            initialValue = 1,
+            allocationSize = 1
+    )
+
+    @Column(name = "ID_PASSAGEIRO")
+    private Long id;
+
+    @OneToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(
+            name = "PESSOA",
+            referencedColumnName = "ID_PESSOA",
+            foreignKey = @ForeignKey(name = "FK_PESSOA_PASSAGEIRO")
+    )
+    private Pessoa pessoa;
+}
