635b57fd956449a5f06de737719d23483c3f05c6
ribeiromurilo <550858@fiap.com.br>
2024-03-19 19:38:35 -0300

    mapping (teste)

--- src/main/java/br/com/fiap/springpfentregas/SpringPfEntregasApplication.java
+++ src/main/java/br/com/fiap/springpfentregas/SpringPfEntregasApplication.java
@@ -2,7 +2,10 @@ package br.com.fiap.springpfentregas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringPfEntregasApplication {
 
@@ -10,4 +13,8 @@ public class SpringPfEntregasApplication {
         SpringApplication.run( SpringPfEntregasApplication.class, args );
     }
 
+    @GetMapping(value = "/")
+    public String index(){
+        return "Site";
+    }
 }
