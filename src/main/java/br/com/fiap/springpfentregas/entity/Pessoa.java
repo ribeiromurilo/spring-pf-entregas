14935e7fef95c68efe81e0153c7ab421ec185038
Murilo <rm550858@fiap.com.br>
2024-03-19 09:09:31 -0300

    create class Pessoa

---
+++ src/main/java/br/com/fiap/springpfentregas/entity/Pessoa.java
@@ -0,0 +1,36 @@
+package br.com.fiap.springpfentregas.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "TB_CP1_PESSOA")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_PESSOA")
+    @SequenceGenerator(
+            name = "SQ_PESSOA",
+            sequenceName = "SQ_PESSOA",
+            initialValue = 1,
+            allocationSize = 1
+    )
+    @Column(name = "ID_PESSOA")
+    private Long id;
+    @Column(name = "NM_PESSOA")
+    private String nome;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "NASCIMENTO")
+    private LocalDate nascimento;
+
+}
